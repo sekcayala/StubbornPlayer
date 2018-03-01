d4bd56b77debe7a6a092ab311c80e4c3c058d47d
sekcayala <aayala39@mail.csuchico.edu>
2018-02-28 18:58:23 -0800

    think it works

--- main.cpp
+++ main.cpp
@@ -6,30 +6,22 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-   
-  game.newGame() ;
+  
+  int wins = 0;
   int amount;
   cout<<"How many times do you want to play this game?\n";
   cin>>amount;
-
+  
   for (int j=0; j < amount; j++)
   {
   game.newGame();
-  cout<<"Which door do you choose? (A/B/C)\n";
   game.guessDoorC();
-  game.newGame();
-  cout<<"Try again for your second choice.\n";
   game.guessDoorC();
-   int wins;
    if (game.isWinner() == true)
    {
-   cout<<wins++;
+   wins++;
    }
-   else 
-   {
-   cout<<"You lose\n";
-   }
-   j++;
   }
+  cout<<"wins: "<<wins<<endl;
   return 0;
 }
